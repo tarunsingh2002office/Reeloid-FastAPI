696dc2821755967d6e31623a3056cea0f0cae52d
SOFT-14 <tarunsingh2002office@gmail.com>
2025-04-16 09:54:04 +0530

    unstable 1.0.9 version

--- users/views/googleAuth.py
+++ users/views/googleAuth.py
@@ -11,24 +11,23 @@ from helper_function.saveUserInDataBase import saveUserInDataBase
 from helper_function.updateLoginStatus import updateLoginStatus
 from helper_function.serialize_mongo_document import serialize_document
 
-async def googleAuth(request:Request,body: dict = Body(
+async def googleAuth(request: Request, body: dict = Body(
         example={
-            "fcmtoken":"1234",
+            "fcmtoken": "1234",
             "deviceType": "web",
-            "authToken":"1234"
+            "authToken": "1234"
         }
     )):
     try:
         body = await request.json()
     except json.JSONDecodeError:
         return JSONResponse({"msg": "Invalid JSON"}, status_code=400)
+
     fcmtoken = body.get("nId")  # notification id
     deviceType = body.get("deviceType")
-
     authToken = body.get("authToken")
 
     try:
-
         CLIENT_ID = google_settings.GOOGLE_CLIENT_ID
         idinfo = id_token.verify_oauth2_token(
             authToken, requests.Request(), CLIENT_ID
@@ -38,9 +37,9 @@ async def googleAuth(request:Request,body: dict = Body(
         current_time = time.time()
 
         if idinfo and issuer == "https://accounts.google.com":
-
             if expiration_time < current_time:
                 raise ValueError("token is expired")
+
             email = idinfo.get("email")
             userResponse = users_collection.find_one(
                 {"email": email}, {"password": 0}
@@ -48,7 +47,9 @@ async def googleAuth(request:Request,body: dict = Body(
             name = idinfo.get("name")
 
             if userResponse:
+                # Serialize the userResponse document
                 userResponse = serialize_document(userResponse)
+
                 updatedUserResponse, token = updateLoginStatus(
                     userResponse, fcmtoken, deviceType
                 )
@@ -56,12 +57,11 @@ async def googleAuth(request:Request,body: dict = Body(
                 return JSONResponse(
                     {
                         "msg": "google authentication done......user is already registered with us",
-                        "userData": updatedUserResponse,
+                        "userData": serialize_document(updatedUserResponse),
                         "token": token,
                     }
                 )
             else:
-
                 password = ""
                 saveUserInDataBase(
                     {"name": name, "email": email, "password": password}
@@ -69,9 +69,10 @@ async def googleAuth(request:Request,body: dict = Body(
                 getSavedUser = users_collection.find_one(
                     {"email": email}, {"password": 0}
                 )
-                # print("get",getSavedUser)
                 if getSavedUser:
+                    # Serialize the getSavedUser document
                     getSavedUser = serialize_document(getSavedUser)
+
                     updatedUserResponse, token = updateLoginStatus(
                         getSavedUser, fcmtoken, deviceType
                     )
@@ -79,7 +80,7 @@ async def googleAuth(request:Request,body: dict = Body(
                     return JSONResponse(
                         {
                             "msg": "google authentication done......registered a new account",
-                            "userData": updatedUserResponse,
+                            "userData": serialize_document(updatedUserResponse),
                             "token": token,
                         }
                     )
