add779f763b4cddb58eb910e41fecaeb18bdf2e9
SOFT-14 <tarunsingh2002office@gmail.com>
2025-04-16 11:55:06 +0530

    unstable 1.0.14 version

--- sliders/views/getDataRelatedToLayouts.py
+++ sliders/views/getDataRelatedToLayouts.py
@@ -1,9 +1,10 @@
 from bson import ObjectId
-from fastapi import Request
+from fastapi import Request, Body, Depends
 from fastapi.responses import JSONResponse
 from core.database import layouts_collection, movies_collection
-
-async def getDataRelatedToLayOuts(request:Request, layoutID):
+from helper_function.apis_requests import get_current_user
+async def getDataRelatedToLayOuts(request:Request, layoutID: str, 
+                                   token: str = Depends(get_current_user)):
 
     result = layouts_collection.find({"_id": ObjectId(layoutID), "visible": True})
     movieObj = []
