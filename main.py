7bc07272f8be78633aa5d5a4f8d02df12f04aab1
SOFT-14 <tarunsingh2002office@gmail.com>
2025-04-18 11:36:09 +0530

    unstable 1.0.27 version - Fixing double api routes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from core.routes import api_router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Request
 from middleware.tokenAuthentication import AccessTokenAuthenticatorMiddleware
 from middleware.allowedHostsMiddleware import AllowedHostsMiddleware
 
@@ -11,7 +12,8 @@ origins = [
     "http://localhost:3000",
     "http://192.168.1.64:8000",
     "http://127.0.0.1:3000",
-    "http://3.110.39.32:8000",
+    "http://3.110.39.32:8000"
+    # "*"
 ]
 allowed_hosts=["*"]   # allowed_hosts=["localhost", "127.0.0.1", "example.com"]
 
@@ -22,11 +24,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["token", "content-type"],
 )
+
+# @app.middleware("http")
+# async def log_requests(request: Request, call_next):
+#     print(f"Request: {request.method} {request.url}")
+#     response = await call_next(request)
+#     return response
+
 # Note that in FastAPI, You can also specify allow_credentials=True to allow credentials in CORS requests.
 app.add_middleware(AllowedHostsMiddleware, allowed_hosts)
 # Add the custom middleware
 app.add_middleware(AccessTokenAuthenticatorMiddleware)
-
+# app.add_middleware(log_requests)
 # Include all routes
 app.include_router(api_router)
 
@@ -34,4 +43,6 @@ app.include_router(api_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True) 
+
+    # uvicorn main:app --host 0.0.0.0 --port 8000 --reload
