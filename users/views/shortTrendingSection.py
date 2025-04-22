e4a39933edb1603927382bd5b7d371ef8080d08b
SOFT-14 <tarunsingh2002office@gmail.com>
2025-04-22 10:44:49 +0530

    unstable 1.0.37 version - code optimization of user/trendingTrailers

--- users/views/shortTrendingSection.py
+++ users/views/shortTrendingSection.py
@@ -2,31 +2,58 @@ from fastapi import Request, Depends
 from fastapi.responses import JSONResponse
 from core.database import movies_collection, shorts_collection
 from helper_function.apis_requests import get_current_user
-async def TrailerTrendingSection(request:Request,token: str = Depends(get_current_user)):
 
-    moviesData = (
-        movies_collection.find(
-            {},
-            {"_id": 1, "name": 1, "shorts": 1, "trailerUrl": 1, "fileLocation": 1},
-        )
-        .sort("views", -1)
-        .limit(10)
-    )
-    moviesArray = []
-    async for movie in moviesData:
+async def TrailerTrendingSection(request: Request, token: str = Depends(get_current_user)):
+    try:
+        # Build aggregation pipeline to fetch top 10 trailers with their visible shorts
+        pipeline = [
+            {"$sort": {"views": -1}},
+            {"$limit": 10},
+            {"$project": {
+                "_id": 1,
+                "name": 1,
+                "views": 1,       
+                "shorts": 1,
+                "trailerUrl": 1,
+                "fileLocation": 1
+            }},
+            {"$unwind": {"path": "$shorts", "preserveNullAndEmptyArrays": True}},
+            {"$match": {"shorts": {"$ne": "Ads"}}},
+            {"$lookup": {
+                "from": shorts_collection.name,
+                "let": {"shortId": "$shorts"},
+                "pipeline": [
+                    {"$match": {"$expr": {"$eq": ["$_id", "$$shortId"]}}},
+                    {"$match": {"visible": True}},
+                    {"$project": {"_id": {"$toString": "$_id"}, "name": 1, "fileLocation": 1}}
+                ],
+                "as": "shortData"
+            }},
+            {"$unwind": {"path": "$shortData", "preserveNullAndEmptyArrays": True}},
+            {"$group": {
+                "_id": "$_id",
+                "name": {"$first": "$name"},
+                "views": {"$first": "$views"},     
+                "trailerUrl": {"$first": "$trailerUrl"},
+                "fileLocation": {"$first": "$fileLocation"},
+                "shorts": {"$push": "$shortData"}
+            }},
+            {"$sort": {"views": -1}},
+            {"$project": {
+                "_id": {"$toString": "$_id"},
+                "name": 1,
+                "trailerUrl": 1,
+                "fileLocation": 1,
+                "shorts": 1
+            }}
+        ]
 
-        movie["_id"] = str(movie["_id"])
-        shortsArray = []
-        for shortid in movie["shorts"]:
-            if shortid != "Ads":
-                shortsData = await shorts_collection.find_one(
-                    {"_id": shortid, "visible": True},
-                    {"_id": 1, "name": 1, "fileLocation": 1},
-                )
-                if shortsData:
-                    shortsData["_id"] = str(shortsData["_id"])
-                    shortsArray.append(shortsData)
+        # Execute aggregation
+        cursor = movies_collection.aggregate(pipeline)
+        trailers = []  
+        async for doc in cursor:
+            trailers.append(doc)
 
-        movie["shorts"] = shortsArray
-        moviesArray.append(movie)
-    return JSONResponse({"trailersData": moviesArray}, status_code=200)
+        return JSONResponse({"trailersData": trailers}, status_code=200)
+    except Exception as e:
+        return JSONResponse({"error": str(e)}, status_code=500)
