472506b28349e6fea9e69459777c3b0498db666a
SOFT-14 <tarunsingh2002office@gmail.com>
2025-04-23 16:54:52 +0530

    unstable testing version 1.1

--- core/load_test.py
+++ core/load_test.py
@@ -1,99 +1,11 @@
-# import locust
-# from locust import HttpUser, TaskSet, task, between
-# import random
-# import json
-
-# class UserBehavior(TaskSet):
-#     def on_start(self):
-#         """Simulate user creation and login to obtain JWT token."""
-#         # Generate a unique email for each user
-#         unique_email = f"user{random.randint(1000, 9999)}@example.com"
-#         password = "password123"
-
-#         # Step 1: Create a user
-#         create_user_response = self.client.post(
-#             "/user/register",
-#             json={
-#                 "email": unique_email,
-#                 "name": "Test User",
-#                 "password": password,
-#                 "confirmPassword": password
-#             }
-#         )
-
-#         if create_user_response.status_code == 200:
-#             # Step 2: Sign in with the created user to obtain JWT token
-#             sign_in_response = self.client.post(
-#                 "/user/signIn",
-#                 json={"email": unique_email, "password": password}
-#             )
-#             if sign_in_response.status_code == 200:
-#                 self.jwt_token = sign_in_response.json().get("token")
-#             else:
-#                 self.jwt_token = None
-#                 # print(f"Sign-in failed: {sign_in_response.text}")
-#         else:
-#             self.jwt_token = None
-#             # print(f"User creation failed: {create_user_response.text}")
-
-            
-#         @task(3)  # Weight: 3 (more frequent)
-#         def get_trending_movies(self):
-#             """Simulate a GET request to fetch trending movies."""
-#             self.client.get(
-#                 "/user/trendingMovies",
-#                 headers={"token": self.jwt_token}
-#             )
-
-# #     @task(2)  # Weight: 2
-# #     def search_item(self):
-# #         """Simulate a GET request to search for a movie."""
-# #         query = random.choice(["action", "comedy", "drama"])
-# #         self.client.get(
-# #             f"/user/searchItem?name={query}",
-# #             headers={"token": self.jwt_token}
-# #         )
-
-# #     @task(1)  # Weight: 1 (less frequent)
-# #     def update_password(self):
-# #         """Simulate a POST request to update the password."""
-# #         self.client.post(
-# #             "/user/updatePassword",
-# #             headers={"token": self.jwt_token},
-# #             json={"password": "newpassword123", "confirmPassword": "newpassword123"}
-# #         )
-
-# #     @task(2)  # Weight: 2
-# #     def get_genre_list(self):
-# #         """Simulate a GET request to fetch genre list."""
-# #         self.client.get(
-# #             "/user/genreList",
-# #             headers={"token": self.jwt_token}
-# #         )
-
-# #     @task(1)  # Weight: 1
-# #     def create_user(self):
-# #         """Simulate a POST request to create a new user."""
-# #         self.client.post(
-# #             "/user/register",
-# #             json={
-# #                 "email": f"user{random.randint(1000, 9999)}@example.com",
-# #                 "name": "Test User",
-# #                 "password": "password123",
-# #                 "confirmPassword": "password123"
-# #             }
-# #         )
-
-# class WebsiteUser(HttpUser):
-#     tasks = [UserBehavior]
-#     wait_time = between(1, 5)  # Simulate a wait time between requests (1-5 seconds)
-#     host = "http://localhost:8000/"
-
-
 from locust import HttpUser, TaskSet, task, between
 import random
+import string
 
 class UserBehavior(TaskSet):
+    allGenreIDs = ["670f5038b6a595b05f97336e","670f55b6b6a595b05f973398","670f5092b6a595b05f973374","670f5065b6a595b05f973372","670f5051b6a595b05f973370"]
+    allLanguageIDs = ["6756daf4ff91008fc67fa749","670f50a4b6a595b05f97337b","670f509eb6a595b05f973379","670f509ab6a595b05f973377"]
+
     def on_start(self):
         unique_email = f"user{random.randint(1000, 9999)}@example.com"
         password = "password123"
@@ -127,6 +39,7 @@ class UserBehavior(TaskSet):
                 "/user/trendingMovies",
                 headers={"token": self.jwt_token}
             )
+
     @task(2)
     def test_genreList(self):
         if self.jwt_token:
@@ -134,77 +47,123 @@ class UserBehavior(TaskSet):
                 "/user/genreList",
                 headers={"token": self.jwt_token}
             )
-    # @task(3)
-    # def test_genreSelector(self):
-        
-    # @task(3)
-    # def test_languageList(self):
-        
-    # @task(3)
-    # def test_languageSelector(self):
-        
-    # @task(3)
-    # def test_trendingMovies(self):
-    
-    # @task(3)
-    # def test_trendingTrailers(self):
-    
-    # @task(3)
-    # def test_getUserDetails(self):
 
-    # @task(3)
-    # def test_searchItem(self):
-    
-    # @task(3)
-    # def test_checkInTask(self):
-    
-    # @task(3)
-    # def test_collectCheckIn(self):
-    
-    # @task(3)
-    # def test_markBookMark(self):
-    
-    # @task(3)
-    # def test_getBookMark(self):
-    
-    # @task(3)
-    # def test_likeVideo(self):
-    
-    # @task(3)
-    # def test_getAds(self):
-    
-    # @task(3)
-    # def test_(self):
-    
-    # @task(3)
-    # def test_getPackage(self):
-    
-    # @task(3)
-    # def test_googleAuth(self):
+    @task(3)
+    def test_genreSelector(self):
+        count = random.randint(1, len(self.allGenreIDs))
+        randomGenresSelected = random.sample(self.allGenreIDs, k=count)
+        self.client.post(
+            "/user/genreSelection",
+            headers={"token": self.jwt_token},
+            json={"selectedGenre": randomGenresSelected}
+        )
 
-    # @task(3)
-    # def test_fetchWallet(self):
-    
-    # @task(3)
-    # def test_forgotPassword(self):
-    
-    # @task(3)
-    # def test_verifyOtp(self):
-    
-    # @task(3)
-    # def test_updatePassword(self):
-    
-    # @task(3)
-    # def test_mintsPurchaseHistory(self):
+    @task(2)
+    def test_languageList(self):
+        if self.jwt_token:
+            self.client.get(
+                "/user/languageList",
+                headers={"token": self.jwt_token}
+            )
 
-    # @task(3)
-    # def test_continueWatching(self):
+    @task(3)
+    def test_languageSelector(self):
+        count = random.randint(1, len(self.allLanguageIDs))
+        randomLanguageSelected = random.sample(self.allLanguageIDs, k=count)
+        self.client.post(
+            "/user/languageSelector",
+            headers={"token": self.jwt_token},
+            json={"selectedGenre": randomLanguageSelected}
+        )
+        
+    @task(2)
+    def test_trendingMovies(self):
+        if self.jwt_token:
+            self.client.get(
+                "/user/trendingMovies",
+                headers={"token": self.jwt_token}
+            )
     
-    # @task(3)
-    # def test_getContinueWatching(self):
+    @task(2)
+    def test_trendingTrailers(self):
+        if self.jwt_token:
+            self.client.get(
+                "/user/trendingTrailers",
+                headers={"token": self.jwt_token}
+            )
+
+    @task(2)
+    def test_getUserDetails(self):
+        if self.jwt_token:
+            self.client.get(
+                "/user/getUserDetails",
+                headers={"token": self.jwt_token}
+            )
+
+    @task(3)
+    def test_editUserDetails(self):
+        self.client.post(
+            "/user/editUserDetails",
+            headers={"token": self.jwt_token},
+            json={
+                "email": "updated@gmail.com",
+                "name": "Test User",
+                "password": "password1234",
+                "confirmPassword": "password123"
+            }
+        )
+
+    @task(2)
+    def test_searchItem(self):
+        search_query = ''.join(random.choices(string.ascii_lowercase, k=2))
+        self.client.get(
+            "/user/searchItem",
+            params={"name": search_query},
+            headers={"token": self.jwt_token}
+        )
     
-    # @task(3)
-    # def test_unlikeVideo(self):
+    @task(3)
+    def test_checkInTask(self):
+        self.client.post(
+            "/user/checkInTask",
+            headers={"token": self.jwt_token},
+        )
+    
+    @task(3)
+    def test_collectCheckIn(self):
+    
+    @task(3)
+    def test_markBookMark(self):
+    
+    @task(2)
+    def test_getBookMark(self):
+    
+    @task(3)
+    def test_likeVideo(self):
+    
+    @task(2)
+    def test_getAds(self):
+    
+    @task(2)
+    def test_getPackage(self):
+
+    @task(2)
+    def test_fetchWallet(self):
+    
+    @task(3)
+    def test_forgotPassword(self):
+    
+    @task(2)
+    def test_mintsPurchaseHistory(self):
+
+    @task(3)
+    def test_continueWatching(self):
+    
+    @task(2)
+    def test_getContinueWatching(self):
+    
+    @task(3)
+    def test_unlikeVideo(self):
 
 class WebsiteUser(HttpUser):
     tasks = [UserBehavior]
